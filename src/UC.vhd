ba146200019ff0823fcf5f25bbe4ca7cb7097dd9
NicoletJonathan <jonathannicolet@icloud.com>
2026-06-10 20:36:53 +0200

    UC manuel

--- src/UC.vhd
+++ src/UC.vhd
@@ -30,6 +30,24 @@ entity UC is
 
 
         -- to be complted
+		  cap_l_i               : in  std_logic;
+		  cap_m_i               : in  std_logic;
+		  cap_r_i               : in  std_logic;
+		  mode_i                : in  std_logic;
+		  start_i               : in  std_logic;
+		  init_i                : in  std_logic;
+		  nb_tour_i             : in  std_logic;
+		  run_l_i               : in  std_logic;
+		  run_m_i               : in  std_logic;
+		  run_r_i               : in  std_logic;
+
+		  en_mot_l_o            : out std_logic;
+		  dir_mot_l_o           : out std_logic;
+		  en_mot_m_o            : out std_logic;
+		  dir_mot_m_o           : out std_logic;
+		  en_mot_r_o            : out std_logic;
+		  dir_mot_r_o           : out std_logic;
+		  sel_speed_o           : out std_logic_vector(1 downto 0)
 
 
 
@@ -51,7 +69,11 @@ architecture fsm of UC is
 
 
         -- Mode Manual
-        MAN_.....,
+        MAN_STOP,
+		  MAN_LEFT,
+		  MAN_RIGHT,
+		  MAN_BOTH,
+		  MAN_MIDDLE,
 
         -- Mode Automatique
 
@@ -69,9 +91,19 @@ architecture fsm of UC is
     signal next_state_s  : state_t;
 
     -- internal signals
+	 
+	 
 
 begin
 
+		en_mot_l_o  <= '0';
+		en_mot_m_o  <= '0';
+		en_mot_r_o  <= '0';
+		dir_mot_l_o <= '0';
+		dir_mot_m_o <= '0';
+		dir_mot_r_o <= '0';
+		sel_speed_o <= "00";
+		next_state_s <= INIT;
     --| Update state proc |----------------------------------------------------
     -- This process update the state of the state machine
     fsm_reg : process(clk_i, rst_i) is
@@ -109,9 +141,90 @@ begin
 
 
         --| Manual sequence |--------------------------------------------------
-
-
-
+				when MAN_STOP =>
+					en_mot_l_o <= '0';
+					en_mot_m_o <= '0';
+					en_mot_r_o <= '0';
+					
+					if(cap_m_i = '0' and run_l_i = '1' and run_m_i = '0' and run_r_i = '0') then
+						next_state_s <= MAN_LEFT;
+					elsif(cap_m_i = '0' and run_l_i = '0' and run_m_i = '0' and run_r_i = '1') then
+						next_state_s <= MAN_RIGHT;
+					elsif(cap_m_i = '0' and run_l_i = '1' and run_m_i = '0' and run_r_i = '1') then
+						next_state_s <= MAN_BOTH;
+					elsif(cap_l_i = '0' and cap_r_i = '0' and run_l_i = '0' 
+							and run_m_i = '1' and run_r_i = '0') then
+						next_state_s <= MAN_MIDDLE;
+					else
+						next_state_s <= MAN_STOP;
+					end if;
+				
+				when MAN_LEFT =>
+					en_mot_l_o <= '1';
+					en_mot_m_o <= '0';
+					en_mot_r_o <= '0';
+					
+					dir_mot_l_o <= '0';
+					sel_speed_o <= "00";
+					
+					if(cap_m_i = '1' or run_l_i = '0' or run_m_i = '1') then
+						next_state_s <= MAN_STOP;
+					elsif(cap_m_i = '0' and run_l_i = '1' and run_m_i = '0' and run_r_i = '1') then
+						next_state_s <= MAN_BOTH;
+					else
+						next_state_s <= MAN_LEFT;
+					end if;
+					
+				when MAN_BOTH =>
+					en_mot_l_o <= '1';
+					en_mot_m_o <= '0';
+					en_mot_r_o <= '1';
+					
+					dir_mot_l_o <= '0';
+					dir_mot_r_o <= '0';
+					sel_speed_o <= "00";
+					
+					if(cap_m_i = '1' or run_m_i = '1' or (run_l_i = '0' and run_r_i = '0')) then
+						next_state_s <= MAN_STOP;
+					elsif(cap_m_i = '0' and run_l_i = '1' and run_m_i = '0' and run_r_i = '0') then
+						next_state_s <= MAN_LEFT;
+					elsif(cap_m_i = '0' and run_l_i = '0' and run_m_i = '0' and run_r_i = '1') then
+						next_state_s <= MAN_RIGHT;
+					else
+						next_state_s <= MAN_BOTH;
+					end if;
+					
+				when MAN_RIGHT =>
+					en_mot_l_o <= '0';
+					en_mot_m_o <= '0';
+					en_mot_r_o <= '1';
+					
+					dir_mot_r_o <= '0';
+					sel_speed_o <= "00";
+					
+					if(cap_m_i = '1' or run_r_i = '0' or run_m_i = '1') then
+						next_state_s <= MAN_STOP;
+					elsif(cap_m_i = '0' and run_l_i = '1' and run_m_i = '0' and run_r_i = '1') then
+						next_state_s <= MAN_BOTH;
+					else
+						next_state_s <= MAN_RIGHT;
+					end if;
+					
+				when MAN_MIDDLE =>
+					en_mot_l_o <= '0';
+					en_mot_m_o <= '1';
+					en_mot_r_o <= '0';
+					
+					dir_mot_m_o <= '0';
+					sel_speed_o <= "00";
+					
+					if(cap_l_i = '1' or cap_r_i = '1' or run_l_i = '1' 
+						or run_m_i = '0' or run_r_i = '1') then
+						next_state_s <= MAN_STOP;
+					else
+						next_state_s <= MAN_MIDDLE;
+					end if;
+						
         --| Automatic sequence |-----------------------------------------------
 
 
